ce27d0b732d103b2c2433ecbb48963a6d020dcee
Sailesh1993 <karki.saileshnep@gmail.com>
2024-10-29 22:09:22 +0200

    Second Commit, add the number in the question of python module 9 assignment.

--- module9Assignment.py
+++ module9Assignment.py
@@ -1,4 +1,4 @@
-# Write a Car class that has the following properties: registration number, maximum speed, current speed and travelled distance.
+# 1. Write a Car class that has the following properties: registration number, maximum speed, current speed and travelled distance.
 # Add a class initializer that sets the first two of the properties based on parameter values.
 # The current speed and travelled distance of a new car must be automatically set to zero.
 # Write a main program where you create a new car (registration number ABC-123, maximum speed 142 km/h).
@@ -24,7 +24,7 @@ print(car2)
 '''
 from random import random, randint
 
-# Extend the program by adding an accelerate method into the new class.
+# 2.  Extend the program by adding an accelerate method into the new class.
 # The method should receive the change of speed (km/h) as a parameter.
 # If the change is negative, the car reduces speed.
 # The method must change the value of the speed property of the object.
